61f2de0cdca9aab700ca2516c95fe53ecaee3317
Martin Funkquist <martin.funkquist@gmail.com>
2026-02-19 12:31:52 +0100

    Queueing multiple experiments

--- queue_experiment.sh
+++ queue_experiment.sh
@@ -1,9 +1,23 @@
 
 EXPERIMENTS_FOLDER="/work/rleap1/martin.funkquist/experiments/ALCSAT"
-INPUT_FOLDER=/work/rleap1/martin.funkquist/ALCSAT/tests/C3
-INPUT_FOLDER_NAME=$(basename "$INPUT_FOLDER")
-OUTPUT_FOLDER="$EXPERIMENTS_FOLDER/$INPUT_FOLDER_NAME"
-mkdir -p "$OUTPUT_FOLDER"
-pushd ${OUTPUT_FOLDER} > /dev/null
-sbatch -J "ALCSAT-C3" run_spell.sh "$INPUT_FOLDER"
-popd > /dev/null
+REPO_FOLDER="/work/rleap1/martin.funkquist/ALCSAT"
+
+queue_experiment () {
+    local INPUT_FOLDER="$1"
+
+    INPUT_FOLDER_NAME=$(basename "$INPUT_FOLDER")
+    OUTPUT_FOLDER="$EXPERIMENTS_FOLDER/$INPUT_FOLDER_NAME"
+    mkdir -p "$OUTPUT_FOLDER"
+
+    pushd ${OUTPUT_FOLDER} > /dev/null
+
+    echo "Queuing job for input folder: $INPUT_FOLDER"
+    sbatch -J "ALCSAT-$INPUT_FOLDER_NAME" "$REPO_FOLDER/run_spell.sh" "$INPUT_FOLDER"
+
+    popd > /dev/null
+}
+
+queue_experiment "$REPO_FOLDER/tests/C3"
+queue_experiment "$REPO_FOLDER/tests/C3-2"
+queue_experiment "$REPO_FOLDER/tests/C3-2-eq"
+queue_experiment "$REPO_FOLDER/tests/C3-10-eq"
