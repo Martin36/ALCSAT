15bd3aecf7049846cc2b5dc73d3d8e39c37b67ae
Martin Funkquist <martin.funkquist@gmail.com>
2026-02-20 10:47:15 +0100

    Added gripper example with all the states

--- queue_experiment.sh
+++ queue_experiment.sh
@@ -29,14 +29,17 @@ queue_experiment () {
     popd > /dev/null
 }
 
-queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_6/C3-6"
-queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_7/C3-7"
-queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_8/C3-8"
-queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_9/C3-9"
-queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_10/C3-10"
+# queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_6/C3-6"
+# queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_7/C3-7"
+# queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_8/C3-8"
+# queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_9/C3-9"
+# queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_10/C3-10"
+queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic-all/C3"
 
-queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_6/C6-6"
-queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_7/C6-7"
-queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_8/C6-8"
-queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_9/C6-9"
-queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_10/C6-10"
+
+# queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_6/C6-6"
+# queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_7/C6-7"
+# queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_8/C6-8"
+# queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_9/C6-9"
+# queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic_10/C6-10"
+queue_experiment "$REPO_FOLDER/tests/gripper/gripper-atomic-all/C6"
